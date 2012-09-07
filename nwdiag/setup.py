5ec757a00e26c6603e809cf8c42f6e37c7a58f3f
tk0miya <i.tkomiya@gmail.com>
2012-09-08 00:49:50 +0900

    Bump version

--- nwdiag/setup.py
+++ nwdiag/setup.py
@@ -42,11 +42,11 @@ And insert rack structure diagrams::
 This module needs nwdiag_.
 '''
 
-requires = ['nwdiag>=0.7.0', 'Sphinx>=0.6']
+requires = ['nwdiag>=0.8.1', 'Sphinx>=0.6']
 
 setup(
     name='sphinxcontrib-nwdiag',
-    version='0.4.2',
+    version='0.5.0',
     url='http://bitbucket.org/birkenfeld/sphinx-contrib',
     download_url='http://pypi.python.org/pypi/sphinxcontrib-nwdiag',
     license='BSD',
