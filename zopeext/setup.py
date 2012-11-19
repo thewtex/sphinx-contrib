c0b106ce0b3ce5501769a8dfd70f3648368fea70
Michael McNeil Forbes <mforbes@physics.ubc.ca>
2012-11-19 12:35:26 -0800

    Added Framework for zopeext to support pip.

--- zopeext/setup.py
+++ zopeext/setup.py
@@ -51,6 +51,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Framework :: Zope3',
         'Topic :: Documentation',
         'Topic :: Utilities',
     ],
